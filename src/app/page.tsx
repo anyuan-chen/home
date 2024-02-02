8a03556fb9a6c97a020919a652db9ed0a451eeca
Andrew Chen <andrew.chen.anyuan@gmail.com>
2024-02-02 12:54:45 -0500

    temp front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,38 +21,9 @@ export default function DialogDemo() {
           </div>
         </BorderCard>
       </div>
-      <div className="flex gap-8">
-        <BorderCard className="text-5xl p-8 bg-accent-green flex-grow">
-          <div
-            className="flex flex-col gap-y-4 text-2xl items-center"
-            style={{ lineHeight: "1.2" }}
-          >
-            <h2 className="self-start">i love:</h2>
-            <div className="flex flex-col gap-y-4">
-              <span className="block text-5xl font-medium italic ">
-                optimization <span className="not-italic pl-4">🖥️</span>
-              </span>
-              <a
-                href="/snippets"
-                className="text-5xl font-medium italic pl-16 flex items-start"
-              >
-                <UnderlineAnimation>
-                  lyrics in music <span className="not-italic pl-4">🎵</span>{" "}
-                </UnderlineAnimation>
-              </a>
-
-              <span className="text-5xl font-medium italic ml-32 flex items-start">
-                <UnderlineAnimation>
-                  the history of cities{" "}
-                  <span className="not-italic pl-4">🚆</span>
-                </UnderlineAnimation>
-              </span>
-              <span className="block text-5xl font-medium italic pl-64">
-                browsing the internet{" "}
-                <span className="not-italic pl-4">🌐</span>
-              </span>
-            </div>
-          </div>
+      <div className="flex gap-8 ">
+        <BorderCard className="text-5xl p-8 bg-accent-green flex-grow text-gray-400">
+          hi
         </BorderCard>
         <CurrentlyPlaying></CurrentlyPlaying>
       </div>
